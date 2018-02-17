d0d0345c81864fb96dd0d13bbc77cfefe8a41ce1
Leeron Tan <leeron.tan@MacBook-Pro.local>
2018-02-16 18:11:22 -0600

    added responsive calculation

--- calculate.js
+++ calculate.js
@@ -1,3 +1,4 @@
+/* TODO Would be nice if it calculates on the fly */
 function calculate(){
 	var numProducts = 0;
 	var numFeatures = 0;
@@ -11,56 +12,56 @@ function calculate(){
 	var pro_reviews = $('#pro_reviews');
 	var pro_questions = $('#pro_questions');
 
-	if (pro_reviews.is(':checked')){numProducts++};
-	if (pro_questions.is(':checked')){numProducts++};
+	if (pro_reviews.prop("checked")){numProducts++};
+	if (pro_questions.prop("checked")){numProducts++};
 
 	/* Calculate number of features */
 	/* Could actually iterate through this instead */
 	var fea_display = $('#fea_display');
 	var fea_submission = $('#fea_submission');
 
-	if (fea_display.is(':checked')){numFeatures++};
-	if (fea_submission.is(':checked')){numFeatures++};
+	if (fea_display.prop("checked")){numFeatures++};
+	if (fea_submission.prop("checked")){numFeatures++};
 
 	//console.log("Products:" + numProducts);
 	//console.log("Features:" + numFeatures);
 
 	if (numProducts == 2 && numFeatures == 2){
-		baseHours = 80;
-		ieHours = 15;
-		uidHours = 45;
-		ipmHours = 20;
+		baseHours += 80;
+		ieHours += 15;
+		uidHours += 45;
+		ipmHours += 20;
 	} else if (numProducts == 2 && numFeatures == 1){
-		baseHours = 50;
-		ieHours = 5;
-		uidHours = 30;
-		ipmHours = 15;
+		baseHours += 50;
+		ieHours += 5;
+		uidHours += 30;
+		ipmHours += 15;
 	} else if (numProducts == 1 && numFeatures == 2){
-		baseHours = 60;
-		ieHours = 10;
-		uidHours = 35;
-		ipmHours = 15;
+		baseHours += 60;
+		ieHours += 10;
+		uidHours += 35;
+		ipmHours += 15;
 	} else if (numProducts == 1 && numFeatures == 1){
-		baseHours = 40;
-		ieHours = 5;
-		uidHours = 25;
-		ipmHours = 10;
+		baseHours += 40;
+		ieHours += 5;
+		uidHours += 25;
+		ipmHours += 10;
 	}
 
 	//console.log("Base Hours: " + baseHours);
 
 	/* Calculate Override Hours*/
-	var overrideCount = $('#override_count').value;
+	var overrideCount = $('#override_count').val();
 	var override_std = $('#override_std');
 	var override_high = $('#override_high');
+	var overrideHours = 0;
 	var stdMultiplier = 2; //# of hours for one standard override/custom styling 
 	var highMultiplier = 5; //# of hours for one high level override/custom styling
 
 	if (overrideCount != null){
-		var overrideHours = 0;
-		if (override_std.is(':checked')){
+		if (override_std.prop("checked")){
 			overrideHours = overrideCount * stdMultiplier;
-		} else if (override_high.is(':checked')){
+		} else if (override_high.prop("checked")){
 			overrideHours = overrideCount * highMultiplier;
 		}
 		ieHours += overrideHours;
@@ -70,15 +71,16 @@ function calculate(){
 	//console.log("Override Hours: " + overrideHours);
 
 	/* Calculate Custom Styling Hours */
-	var stylingCount = $('#styling_count').value;
+	var stylingCount = $('#styling_count').val();
 	var styling_std = $('#styling_std');
 	var styling_high = $('#styling_high');
+	var stylingHours = 0;
 
 	if (stylingCount != null){
-		var stylingHours = 0;
-		if (styling_std.is(':checked')){
+		
+		if (styling_std.prop("checked")){
 			stylingHours = stylingCount * stdMultiplier;
-		} else if (styling_high.is(':checked')){
+		} else if (styling_high.prop("checked")){
 			stylingHours = stylingCount * highMultiplier;
 		}
 		uidHours += stylingHours;
@@ -87,8 +89,72 @@ function calculate(){
 
 	//console.log("Styling Hours: " + stylingHours);
 
+	/* Calculate Responsive Hours */
+	var scope_resp = $('#scope_resp');
+	var resp_std = $('#resp_std').prop("checked");
+	var resp_high = $('#resp_high').prop("checked");
+
+	if (scope_resp.prop("checked")){
+		if(resp_high){
+			//High Level Responsive
+			if (numProducts == 2 && numFeatures == 2){
+				//RR & QA and Display & Submission
+				baseHours += 70;
+				ieHours += 10;
+				uidHours += 50;
+				ipmHours += 10;
+			} else if (numProducts == 2 && numFeatures == 1){
+				//RR & QA and Display OR Submission
+				baseHours += 50;
+				ieHours += 5;
+				uidHours += 35;
+				ipmHours += 10;
+			} else if (numProducts == 1 && numFeatures == 2){
+				//RR OR QA and Display & Submission
+				baseHours += 40;
+				ieHours += 5;
+				uidHours += 30;
+				ipmHours += 5;
+			} else if (numProducts == 1 && numFeatures == 1){
+				//RR OR QA and Display OR Submission
+				baseHours += 30;
+				ieHours += 5;
+				uidHours += 20;
+				ipmHours += 5;
+			}
+		}
+		else {
+			//Standard Responsive
+			if (numProducts == 2 && numFeatures == 2){
+				//RR & QA and Display & Submission
+				baseHours += 50;
+				ieHours += 10;
+				uidHours += 30;
+				ipmHours += 10;
+			} else if (numProducts == 2 && numFeatures == 1){
+				//RR & QA and Display OR Submission
+				baseHours += 30;
+				ieHours += 5;
+				uidHours += 20;
+				ipmHours += 5;
+			} else if (numProducts == 1 && numFeatures == 2){
+				//RR OR QA and Display & Submission
+				baseHours += 25;
+				ieHours += 5;
+				uidHours += 15;
+				ipmHours += 5;
+			} else if (numProducts == 1 && numFeatures == 1){
+				//RR OR QA and Display OR Submission
+				baseHours += 20;
+				ieHours += 5;
+				uidHours += 10;
+				ipmHours += 5;
+			}
+		}
+	}
+
 	/* Hours Output */
-	totalHours = baseHours + overrideHours + stylingHours;
+	var totalHours = baseHours + overrideHours + stylingHours;
 
 	outputText = "<h4>Overall Hours</h4>"
 	outputText += "<p><b>Base Hours:</b> " + baseHours + "<br>";
@@ -102,5 +168,5 @@ function calculate(){
 	outputText += "<b>UID Hours:</b> " + uidHours + "</p>";
 
 	var output_span = $('#output_span');
-	output_span.append(outputText);
+	output_span.html(outputText);
 }
